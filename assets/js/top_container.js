731cea3241c8db604383b5bbfc0e3da4d1cd8d4b
san-dra <sandra.alemayehu@gmail.com>
2025-05-19 00:54:20 +0300

    contact and responsive layout

--- assets/js/top_container.js
+++ assets/js/top_container.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // === NAVIGATION LINKS from home_meta.json ===
     const basePath = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? ""
-  : "/my-portfolio"; // replace with your actual repo name if it's not username.github.io
+  : ""; // replace with your actual repo name if it's not username.github.io
 
 const navLinks = (home.main_navigation || []).map(link =>
   `<a href="${basePath}/${link.href}">${link.text}</a>`
